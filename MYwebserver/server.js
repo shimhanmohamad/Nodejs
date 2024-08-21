5dc50e1137f5503a0803574cdcc40b7b41076ec3
shimhanmohamad <mohamadshimhan@gmail.com>
2024-08-21 09:04:46 +0530

    server

---
+++ MYwebserver/server.js
@@ -0,0 +1,28 @@
+const http = require('http')
+const PORT = 4000
+const fs = require('fs')
+
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    fs.readFile('index.html', (error,data) => {
+        if (error) {
+            res.writeHead(404)
+            res.write('Page Not Found')
+        }
+        else {
+            res.write(data)
+        }
+        res.end();
+    })
+    
+})
+
+server.listen(PORT, (error) => {
+    if (error) {
+        console.log(error)
+    }
+    else {
+        console.log('server is runninig on ' + PORT)
+    }
+})
